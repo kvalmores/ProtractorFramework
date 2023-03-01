19c67206563cac5e9103ce97c84b2f470185d44c
kvalmores <k.valmores@arcanys.com>
2023-03-01 11:08:09 +0800

    Removed subtraction test

--- tests/calculator.js
+++ tests/calculator.js
@@ -34,21 +34,4 @@ describe('demo calculator tests', function () {
 
     });
 
-
-    // it('subtraction test', function () {
-
-    //     homepage.get('http://juliemr.github.io/protractor-demo/');
-
-    //     homepage.enterFirstNumber('4');
-
-    //     homepage.enterSecondNumber('3');
-
-    //     homepage.clickGo1();
-
-    //     homepage.verifyResult('7');
-
-    //     browser.sleep(3000);
-
-    // });
-
 });
